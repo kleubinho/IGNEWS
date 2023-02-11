53dffe2492dccfa2affb5ff75749a642b0c3d1f5
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-02-11 00:30:44 -0300

    Update

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return <h1>Hello world</h1>
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
+  );
+}
