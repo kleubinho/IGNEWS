3dae948e00f9c13ec6b841b88645bc9244d049a8
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-02-15 13:23:01 -0300

    Update api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -10,13 +12,24 @@ export function SubsribeButton({ priceId }: SubscribeButtonProps) {
 
   console.log('logado',session)
 
-  function handleSubscribe() {
+ async function handleSubscribe() {
     if(!session){
         signIn('github')
         return
-    }
+    }    
 
+    try {
+      const response = await api.post('/subscribe') //nome do arquivo, que sempre será o nome da rota
     
+      const {sessionId} = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout(sessionId)
+    }
+    catch(err){
+      alert(err.message)
+    }
   }
   return (
     <button type="button" className={styles.subscribeButton}>
