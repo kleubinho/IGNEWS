15ed440c014c1482913998f1dbf40bca7e675dde
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-02-20 14:52:19 -0300

    update

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,32 +1,50 @@
-import Head from 'next/head'
-import styles from './styles.module.scss'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
+import * as prismic from '@prismicio/client'
+import { useFirstPrismicDocument } from "@prismicio/react/dist/clientHooks";
 
-export default function Posts(){
-    return(
-        <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-        <main className={styles.container}>
-            <div className={styles.posts}>
-                <a>
-                    <time>12 de março de 2023</time>
-                    <strong>Criando coisas insanas para pessoas insanas</strong>
-                    <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
-                </a>
-                <a>
-                    <time>12 de março de 2023</time>
-                    <strong>Criando coisas insanas para pessoas insanas</strong>
-                    <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
-                </a>
-                <a>
-                    <time>12 de março de 2023</time>
-                    <strong>Criando coisas insanas para pessoas insanas</strong>
-                    <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
-                </a>
-            </div>
-        </main>
-        </>
-    )
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2023</time>
+            <strong>Criando coisas insanas para pessoas insanas</strong>
+            <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2023</time>
+            <strong>Criando coisas insanas para pessoas insanas</strong>
+            <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2023</time>
+            <strong>Criando coisas insanas para pessoas insanas</strong>
+            <p>Nesse guia eu nào sei oque estou fazendo apenas estou fazedno</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const [document] = useFirstPrismicDocument()
+
+//   const response = prismic.usePrismicDocumentByUID('document.type','post')
+
+  console.log(document)
+
+  return {
+    props: {},
+  };
+};
---
+++ src/services/prismic.ts
@@ -0,0 +1,11 @@
+import * as prismic from '@prismicio/client'
+
+export function getPrismicClient() {
+    const client = prismic.createClient(
+        process.env.PRISMIC_ENDPOINT,
+        {
+            accessToken: process.env.PRISMIC_ACCESS_TOKEN
+        }
+    )
+    return client
+}
